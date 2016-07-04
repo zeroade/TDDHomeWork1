9f2d346cd9f7da1f34a4d530165011b6bef32832
Web- <Web-@WEB-BACK-END>
2016-07-04 14:43:11 +0800

    git串接測試

---
+++ TDDHomeWork1/TDDHomeWork1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDHomeWork1
+{
+    public class PagingGroup
+    {
+        public IEnumerable<int> GroupingFieldValues<T>(IEnumerable<T> data, string fieldName, int pagingCount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public int Cost { get; set; }
+        public int Revence { get; set; }
+        public int SellPrice { get; set; }
+    }
+}
---
+++ TDDHomeWork1/TDDHomeWork1Tests/PagingGroupTests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDHomeWork1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDHomeWork1.Tests
+{
+    [TestClass()]
+    public class PagingGroupTests
+    {
+        private IEnumerable<Order> GetOrders() => new[]{
+            new Order{
+                Id=1,
+                Cost = 1,
+                Revence = 11,
+                SellPrice = 21
+            },
+            new Order{
+                Id=2,
+                Cost = 2,
+                Revence = 12,
+                SellPrice = 22
+            },
+            new Order {
+                Id=3,
+                Cost = 3,
+                Revence = 13,
+                SellPrice = 23
+            },
+            new Order{
+                Id=4,
+                Cost = 4,
+                Revence = 14,
+                SellPrice = 24
+            },
+            new Order{
+                Id=5,
+                Cost = 5,
+                Revence = 15,
+                SellPrice = 25
+            },
+            new Order{
+                Id=6,
+                Cost = 6,
+                Revence = 16,
+                SellPrice = 26
+            },
+            new Order{
+                Id=7,
+                Cost = 7,
+                Revence = 17,
+                SellPrice = 27
+            },
+            new Order{
+                Id=8,
+                Cost = 8,
+                Revence = 18,
+                SellPrice = 28
+            },
+            new Order{
+                Id=9,
+                Cost = 9,
+                Revence = 19,
+                SellPrice = 29
+            },
+            new Order{
+                Id=10,
+                Cost = 10,
+                Revence = 20,
+                SellPrice = 30
+            },
+            new Order{
+                Id=11,
+                Cost = 11,
+                Revence = 21,
+                SellPrice = 31
+            }
+        };
+
+        [TestMethod()]
+        public void GroupingFieldValuesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
