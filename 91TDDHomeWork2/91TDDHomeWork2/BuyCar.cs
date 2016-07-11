0c6e02ef72446633ab6f61771158c62b8ab9b174
Web- <Web-@WEB-BACK-END>
2016-07-11 10:52:55 +0800

    提交第一個測試案例"第一集買了一本_其他都沒買_價格應為100元()"

---
+++ 91TDDHomeWork2/91TDDHomeWork2/BuyCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _91TDDHomeWork2
+{
+    public class BuyCar
+    {
+        private double _EndSalePrice = 0;
+        public double compute()
+        {
+            
+            return _EndSalePrice;
+        }
+    }
+}
---
+++ 91TDDHomeWork2/91TDDHomeWork2Tests/BuyCarTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _91TDDHomeWork2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _91TDDHomeWork2.Tests
+{
+    [TestClass()]
+    public class BuyCarTests
+    {
+        [TestMethod()]
+        public void 第一集買了一本_其他都沒買_價格應為100元()
+        {
+            // arrange
+            BuyCar target = new BuyCar();
+            double excepted = 100;
+            // act
+            double actual = target.compute();
+
+            // assert           
+            Assert.AreEqual<double>(excepted, actual);
+        }
+
+        [TestMethod()]
+        public void 第一集買了一本_第二集也買了一本_價格應為190元()
+        {
+            // arrange
+
+            // act
+
+
+            // assert           
+
+        }
+
+        [TestMethod()]
+        public void 一二三集各買了一本_價格應為270元()
+        {
+            // arrange
+
+            // act
+
+
+            // assert           
+
+        }
+
+        [TestMethod()]
+        public void 一次買了整套_價格應為375元()
+        {
+            // arrange
+
+            // act
+
+
+            // assert           
+
+        }
+
+        [TestMethod()]
+        public void 一二三四集各買了一本_價格應為320元()
+        {
+            // arrange
+
+            // act
+
+
+            // assert           
+
+        }
+
+        [TestMethod()]
+        public void 一二集各買了一本_第三集買了兩本_價格應為370元()
+        {
+            // arrange
+
+            // act
+
+
+            // assert           
+
+        }
+
+        [TestMethod()]
+        public void 第一集買了一本_第二三集各買了兩本_價格應為460元()
+        {
+            // arrange
+
+            // act
+
+
+            // assert           
+
+        }
+    }
+}
